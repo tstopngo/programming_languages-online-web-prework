84da8b49f0a74e35fdbd4e568913eb6570b58f0c
Tracy Ngo <tngo1206@gmail.com>
2019-03-07 20:37:02 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+
+  reformated_hash = {}
+    languages.each do |type, languages_hash|
+        languages_hash.each do |language, data|
+          if reformated_hash.has_key?(language)
+          reformated_hash[language][:style] << type
+          else
+          reformated_hash[language] = data
+          reformated_hash[language][:style] = [type]
+          end
+      end
+    end
+  reformated_hash
 end
+
