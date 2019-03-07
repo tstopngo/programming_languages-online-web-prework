3ffc556232aa4c3ca5224f428bc3734cc4a7c51e
Tracy Ngo <tngo1206@gmail.com>
2019-03-07 20:30:12 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
 def reformat_languages(languages)
-  # your code here
+
+  reformated_hash = {}
+
+    languages.each do |type, languages_hash| # puts OO and entire hash of languages
+      if type == :oo
+        languages_hash.each do |language, data|
+        reformated_hash[language] = data
+        reformated_hash[language][:style] = []
+        reformated_hash[language][:style] = [] << :oo
+        end
+      else 
+        languages_hash.each do |language, data|
+        reformated_hash[language] = data
+        reformated_hash[language][:style] = []
+        reformated_hash[language][:style] = [] << :functional  
+        end
+    end
+    end
+    
+  reformated_hash
 end
+
